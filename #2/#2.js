c938ad73be546ed68ca24c7a091fa7367b5505d1
horang-e <rmsid1000@gmail.com>
2022-05-13 21:02:04 +0900

    설명추가

--- #2/#2.js
+++ #2/#2.js
@@ -1,11 +1,14 @@
+
+
 function solution(num) {
-  var answer = '';
-  if (num%2 === 0){
-      return answer ="Even"
+  let answer = '';
+  num = 5; 
+  if (num%2 === 0){         //만약 num을 2로나눈 나머지가 0이라면!!
+      return answer ="Even"     //짝수라고 답을 보여준다
   }else{
-      return answer ="Odd"
+      return answer ="Odd"       //아니라면 홀수라고 표시한다.
   }
   return answer;
+
 }
 
-//answer에 대해 
